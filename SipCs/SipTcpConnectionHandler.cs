f8e68e3194c09b7a91cc2d48478322092464a562
Brian Stauff <brian.stauff.github@gmail.com>
2019-07-26 10:41:28 -0500

    Adding SipTcpConnectionHandler

---
+++ SipCs/SipTcpConnectionHandler.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Connections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SipCs
+{
+    public class SipTcpConnectionHandler : ConnectionHandler
+    {
+        public override Task OnConnectedAsync(ConnectionContext connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
